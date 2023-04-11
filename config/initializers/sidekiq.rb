1d5df84ff014b278bd3ced1a32dfc1e650462e4d
Richard Chou <richard@ezyraise.com>
2023-04-11 17:05:07 +1000

    update

--- config/initializers/sidekiq.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0" }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0" }
-end
