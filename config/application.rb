c52e80251a10abfff27fe81ee1113bec70073507
Richard Chou <konami99+buildwithawscopilot@hotmail.com>
2023-05-13 08:58:07 +1000

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Rails70
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.hosts = [
+      /.*/,
       IPAddr.new("0.0.0.0/0"),
       "localhost",
       "ezyraise.ap.ngrok.io",
